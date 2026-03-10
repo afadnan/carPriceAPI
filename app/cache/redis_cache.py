5c65c80308c9b8dfdbc9c9d37f8027ff99f37802
Adnan Ashraf <af.adnan23@gmail.com>
2026-03-10 23:49:15 +0530

    added cache

--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -1,18 +1,22 @@
 import os
 import redis
 from dotenv import load_dotenv
+import json
+from app.core.config import settings
 
 load_dotenv()
 
 REDIS_URL = os.getenv("REDIS_URL")
 
-redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
+redis_client = redis.Redis.from_url(settings.REDIS_URL)
 
 
 def get_cached_prediction(key: str):
     value = redis_client.get(key)
-    return eval(value) if value else None
+    if value:
+        return json.loads(value)
+    return None
 
 
-def set_cached_prediction(key: str, value: dict):
-    redis_client.set(key, str(value))
+def set_cached_prediction(key: str, value: dict,expiry:int =3600):
+    redis_client.setex(key,expiry, json.dumps(value))
