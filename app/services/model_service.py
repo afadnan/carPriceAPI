e23472251cc6d9d169ba8679b36664664fc1b871
Adnan Ashraf <af.adnan23@gmail.com>
2026-03-18 21:55:00 +0530

    modify

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -5,7 +5,6 @@ from app.cache.redis_cache import set_cached_prediction, get_cached_prediction
 
 model = joblib.load(settings.MODEL_PATH)
 
-
 def predict_car_price(data: dict):
     cache_key = " ".join([str(val) for val in data.values()])
     cached = get_cached_prediction(cache_key)
